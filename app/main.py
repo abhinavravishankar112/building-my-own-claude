76d1713db25cb40f68e09f33c2c1764c9d198b45
Abhinav <abhinavravi.shankar.s.112@kalvium.community>
2026-03-17 10:17:03 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,6 +7,26 @@ from openai import OpenAI
 API_KEY = os.getenv("OPENROUTER_API_KEY")
 BASE_URL = os.getenv("OPENROUTER_BASE_URL", default="https://openrouter.ai/api/v1")
 
+TOOLS = [
+    {
+        "type": "function",
+        "function": {
+            "name": "Read",
+            "description": "Read and return the contents of a file",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "file_path": {
+                        "type": "string",
+                        "description": "The path to the file to read",
+                    }
+                },
+                "required": ["file_path"],
+            },
+        },
+    }
+]
+
 
 def main():
     p = argparse.ArgumentParser()
@@ -21,6 +41,7 @@ def main():
     chat = client.chat.completions.create(
         model="anthropic/claude-haiku-4.5",
         messages=[{"role": "user", "content": args.p}],
+        tools=TOOLS,
     )
 
     if not chat.choices or len(chat.choices) == 0:
